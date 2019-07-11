b160c5b166b939c32f412d62dd97308b592c3fc4
Jenny08p <46802465+Jenny08p@users.noreply.github.com>
2019-07-11 13:21:54 -0400

    player controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,20 +3,22 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed;                
+    public float speed;               
 
-    private Rigidbody2D rb2d;        
+    private Rigidbody2D rb2d;     
 
-
+   
     void Start()
     {
+    
         rb2d = GetComponent<Rigidbody2D> ();
     }
 
- 
     void FixedUpdate()
     {
+
         float moveHorizontal = Input.GetAxis ("Horizontal");
+
 
         float moveVertical = Input.GetAxis ("Vertical");
 
